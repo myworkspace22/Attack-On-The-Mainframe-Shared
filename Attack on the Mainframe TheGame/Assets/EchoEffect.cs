81e7af2794fa4ef2f240bd7b2427fafcc29f9b90
Ribbo <61548182+myworkspace22@users.noreply.github.com>
2021-05-24 19:27:42 +0200

    More funky changes

---
+++ Attack on the Mainframe TheGame/Assets/EchoEffect.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EchoEffect : MonoBehaviour
+{
+    private float timeBtwSpawns;
+    public float startTimeBtwSpawns;
+
+    public GameObject echo;
+
+    private void Update()
+    {
+        if(timeBtwSpawns <= 0)
+        {
+            GameObject instance = (GameObject)Instantiate(echo, transform.position, transform.rotation);
+            //Destroy(instance, 8f)
+            timeBtwSpawns = startTimeBtwSpawns;
+        }
+        else
+        {
+            timeBtwSpawns -= Time.deltaTime;
+        }
+        if(Vector2.Distance(transform.position, GetComponent<AIDestinationSetter>().target.position) <= 0.25f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
